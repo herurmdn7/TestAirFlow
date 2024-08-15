e8f375934800b370db05892f6423f88f5ce65227
herurmdn7 <95133689+herurmdn7@users.noreply.github.com>
2024-08-15 08:21:43 +0700

    Add files via upload

---
+++ milestone.py
@@ -0,0 +1,115 @@
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+#from airflow.providers.postgres.operators.postgres import PostgresOperator
+# from airflow.utils.task_group import TaskGroup
+from datetime import datetime
+from sqlalchemy import create_engine #koneksi ke postgres
+import pandas as pd
+
+from elasticsearch import Elasticsearch
+# from elasticsearch.helpers import bulk
+
+def load_csv_to_postgres():
+ 
+    database = "airflow_m3"
+    username = "airflow_m3"
+    password = "airflow_m3"
+    host = "postgres"
+
+    # Membuat URL koneksi PostgreSQL
+    postgres_url = f"postgresql+psycopg2://{username}:{password}@{host}/{database}"
+
+    # Gunakan URL ini saat membuat koneksi SQLAlchemy
+    engine = create_engine(postgres_url)
+    # engine= create_engine("postgresql+psycopg2://airflow:airflow@postgres/airflow")
+    conn = engine.connect()
+
+    df = pd.read_csv('/opt/airflow/dags/P2M3_Heru_data_raw.csv')
+    #df.to_sql(nama_table_db, conn, index=False, if_exists='replace')
+    df.to_sql('table_m3', conn, 
+              index=False, 
+              if_exists='replace')  # M
+    
+
+def ambil_data():
+    '''
+    
+    '''
+    database = "airflow_m3"
+    username = "airflow_m3"
+    password = "airflow_m3"
+    host = "postgres"
+
+    # Membuat URL koneksi PostgreSQL
+    postgres_url = f"postgresql+psycopg2://{username}:{password}@{host}/{database}"
+    # Gunakan URL ini saat membuat koneksi SQLAlchemy
+    engine = create_engine(postgres_url)
+    conn = engine.connect()
+
+    df = pd.read_sql_query("select * from table_testing", conn) #nama table sesuaikan sama nama table di postgres
+    df.to_csv('/opt/airflow/dags/P2M3_Heru_data_raw_new.csv', sep=',', index=False)
+    
+
+
+def preprocessing(): 
+    ''' fungsi untuk membersihkan data
+    
+    '''
+    # pembisihan data
+    data = pd.read_csv("/opt/airflow/dags/P2M3_Heru_data_raw_new.csv")
+
+    # bersihkan data 
+    data.drop_duplicates(inplace=True)
+    # data= data.query('age>=0')
+    data.to_csv('/opt/airflow/dags/P2M3_Heru_data_clean.csv', index=False)
+    
+def upload_to_elasticsearch():
+    es = Elasticsearch("http://elasticsearch:9200")
+    df = pd.read_csv('/opt/airflow/dags/P2M3_Heru_data_raw_clean.csv')
+    
+    for i, r in df.iterrows():
+        doc = r.to_dict()  # Convert the row to a dictionary
+        res = es.index(index="table_testing", id=i+1, body=doc)
+        print(f"Response from Elasticsearch: {res}")
+        
+
+        
+default_args = {
+    'owner': 'Heru', 
+    'start_date': datetime(2023, 12, 24, 12, 00)
+}
+
+with DAG(
+    "P2M3_heru_DAG_hck", #atur sesuai nama project kalian
+    description='Milestone_3',
+    schedule_interval='10 5 10 * *', #atur schedule untuk menjalankan airflow pada 06:30.
+    default_args=default_args, 
+    catchup=False
+) as dag:
+    
+    # Task : 1
+    load_csv_task = PythonOperator(
+        task_id='load_csv_to_postgres',
+        python_callable=load_csv_to_postgres) #sesuaikan dengan nama fungsi yang kalian buat
+    
+    #task: 2
+    ambil_data_pg = PythonOperator(
+        task_id='ambil_data_postgres',
+        python_callable=ambil_data) #
+    
+
+    # Task: 3
+    '''  Fungsi ini ditujukan untuk menjalankan pembersihan data.'''
+    edit_data = PythonOperator(
+        task_id='edit_data',
+        python_callable=preprocessing)
+
+    # Task: 4
+    upload_data = PythonOperator(
+        task_id='upload_data_elastic',
+        python_callable=upload_to_elasticsearch)
+
+    #proses untuk menjalankan di airflow
+    load_csv_task >> ambil_data_pg >> edit_data >> upload_data
+
+
